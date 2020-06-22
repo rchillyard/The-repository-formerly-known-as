59684622393cf7d4b3727fd6fa8bb6d5971a5b44
Sraw <lzyl888@gmail.com>
2020-06-22 17:01:50 -0400

    add unit tests for PureHuskySort

---
+++ src/test/java/edu/neu/coe/huskySort/sort/huskySort/PureHuskySortTest.java
@@ -0,0 +1,46 @@
+package edu.neu.coe.huskySort.sort.huskySort;
+
+import edu.neu.coe.huskySort.sort.BaseHelper;
+import edu.neu.coe.huskySort.sort.huskySortUtils.HuskySortHelper;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.Assert.assertTrue;
+
+public class PureHuskySortTest {
+
+    private final BaseHelper<String> helper = new BaseHelper<>("dummy helper");
+
+    @Test
+    public void testSortString1() {
+        String[] xs = {"Hello", "Goodbye", "Ciao", "Willkommen"};
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskySortHelper.unicodeCoder);
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString2() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskySortHelper.asciiCoder);
+        final int N = 1000;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> r.nextLong() + "");
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+
+    @Test
+    public void testSortString3() {
+        PureHuskySort<String> sorter = new PureHuskySort<>(HuskySortHelper.asciiCoder);
+        final int N = 1000;
+        helper.init(N);
+        final String[] xs = helper.random(String.class, r -> {
+            int x = r.nextInt(1000000000);
+            final BigInteger b = BigInteger.valueOf(x).multiply(BigInteger.valueOf(1000000));
+            return b.toString();
+        });
+        sorter.sort(xs);
+        assertTrue("sorted", helper.sorted(xs));
+    }
+}
