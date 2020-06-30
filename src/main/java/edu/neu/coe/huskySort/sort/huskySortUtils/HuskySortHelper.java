4f801c1604147f276db5dbed0977207b76dad99b
Sraw <lzyl888@gmail.com>
2020-06-30 12:28:32 -0400

    delete reflection and change to use charAt, it is compatible with Java 11+

--- src/main/java/edu/neu/coe/huskySort/sort/huskySortUtils/HuskySortHelper.java
+++ src/main/java/edu/neu/coe/huskySort/sort/huskySortUtils/HuskySortHelper.java
@@ -3,7 +3,6 @@
  */
 package edu.neu.coe.huskySort.sort.huskySortUtils;
 
-import java.lang.reflect.Field;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.nio.LongBuffer;
@@ -281,38 +280,14 @@ public class HuskySortHelper {
     }
 
     private static long stringToLong(String str, int maxLength, int bitWidth, int mask) {
-        if (isPreJava11) try {
-            Field field = String.class.getDeclaredField("value");
-            field.setAccessible(true);
-            return charsToLong((char[]) field.get(str), maxLength, bitWidth, mask);
-        } catch (Exception e) {
-            throw new RuntimeException("Problem encoding String as long", e);
-        }
-        else return bytesToLong(str.getBytes(), maxLength, bitWidth, mask);
-    }
-
-    private static long charsToLong(char[] charArray, int maxLength, int bitWidth, int mask) {
-        final int length = Math.min(charArray.length, maxLength);
+        final int length = Math.min(str.length(), maxLength);
         final int padding = maxLength - length;
         long result = 0L;
         if (((mask ^ 0xFFFF) & 0xFFFF) == 0)
-            for (int i = 0; i < length; i++) result = result << bitWidth | charArray[i];
+            for (int i = 0; i < length; i++) result = result << bitWidth | str.charAt(i);
         else
-            for (int i = 0; i < length; i++) result = result << bitWidth | charArray[i] & mask;
+            for (int i = 0; i < length; i++) result = result << bitWidth | str.charAt(i) & mask;
 
-        result = result << bitWidth * padding;
-        return result;
-    }
-
-    // TEST
-    private static long bytesToLong(byte[] bytes, int maxLength, int bitWidth, int mask) {
-        long result = 0L;
-        final int length = Math.min(bytes.length, maxLength);
-        final int padding = maxLength - length;
-        if (((mask ^ 0xFF) & 0xFF) == 0)
-            for (int i = 0; i < length; i++) result = result << bitWidth | bytes[i];
-        else
-            for (int i = 0; i < length; i++) result = result << bitWidth | bytes[i] & mask;
         result = result << bitWidth * padding;
         return result;
     }
@@ -382,9 +357,9 @@ public class HuskySortHelper {
         return sign == 0 ? result : -result;
     }
 
-    private static Map<String, HuskySequenceCoder<String>> sequenceCoderMap;
+    private static final Map<String, HuskySequenceCoder<String>> sequenceCoderMap;
 
-    /**
+    /*
      * Initialize the sequenceCoderMap.
      */
     static {
